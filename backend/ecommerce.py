f6b8704cee9482fb8959a697de0b619e1e951997
Marcos Fábio <marcaum54@gmail.com>
2018-06-29 01:51:44 -0300

    Level 3 - Done!

--- backend/ecommerce.py
+++ backend/ecommerce.py
@@ -3,26 +3,24 @@ import json
 
 class Ecommerce:
     class Article:
-        def __init__(self, article, quantity):
+        def __init__(self, article, quantity, discount):
             self._id = article.get('id')
             self._name = article.get('name')
             self._price = article.get('price')
             self._quantity = quantity
-
-        def get_id(self):
-            return self._id
-
-        def get_name(self):
-            return self._name
-
-        def get_price(self):
-            return self._price
-
-        def get_quantity(self):
-            return self._quantity
+            self._discount = discount
 
         def total(self):
             return self._price * self._quantity
+
+        def calculate_discount(self):
+            if self._discount is not None:
+                if self._discount.get('type') == 'amount':
+                    return self._quantity * self._discount.get('value')
+                if self._discount.get('type') == 'percentage':
+                    return self.total() * self._discount.get('value') / 100
+
+            return 0
 
     class Cart:
         def __init__(self, cart):
@@ -32,80 +30,99 @@ class Ecommerce:
         def get_id(self):
             return self._id
 
-        def get_articles(self):
-            return self._articles
-
         def add_article(self, article):
             self._articles.append(article)
 
         def total(self):
             total = 0
-            for article in self.get_articles():
+            for article in self._articles:
                 total += article.total()
             return total
 
-        def delivery_fees_calculated(self, delivery_fees):
-            total = self.total()
-            delivery_fee_calculated = 0
-            for delivery_fee in delivery_fees:
-                eligible_transaction_volume = delivery_fee.get('eligible_transaction_volume')
-                min_price = eligible_transaction_volume.get('min_price')
-                max_price = eligible_transaction_volume.get('max_price')
+        def calculate_discounts(self):
+            discounts_calculated = 0
+            for article in self._articles:
+                discounts_calculated += article.calculate_discount()
+            return discounts_calculated
 
-                has_min_and_max_price_rule = min_price is not None and max_price is not None and (min_price <= total < max_price)
-                only_has_min_price_rule = min_price is not None and max_price is None and total >= min_price
-                only_has_max_price_rule = min_price is None and max_price is not None and total < max_price
-
-                if has_min_and_max_price_rule or only_has_min_price_rule or only_has_max_price_rule:
-                    delivery_fee_calculated += delivery_fee.get('price')
-
-            return delivery_fee_calculated
-
-    def __init__(self, level=None):
-        self.loaded_json = None
+    def __init__(self, level):
         self._carts = []
+        self._loaded_json = None
 
-        if level:
-            with open(self.get_file_from_level(level)) as file_content:
-                self.loaded_json = json.load(file_content)
-                self._delivery_fees = self.loaded_json.get('delivery_fees', [])
+        with open(__class__._get_path_file_by_level(level)) as file_content:
+            self._loaded_json = json.load(file_content)
+            self._discounts = self._loaded_json.get('discounts', [])
+            self._delivery_fees = self._loaded_json.get('delivery_fees', [])
 
-                for cart in self.loaded_json.get('carts', []):
-                    new_cart = __class__.Cart(cart)
+            for cart in self._loaded_json.get('carts', []):
+                new_cart = __class__.Cart(cart)
 
-                    for item in cart.get('items'):
-                        article = None
-                        for art in self.loaded_json.get('articles', []):
-                            if art.get('id') == item.get('article_id'):
-                                article = art
-                                continue
+                for item in cart.get('items'):
+                    article = self._get_article_by_id(id=item.get('article_id'))
+                    if article:
+                        quantity = item.get('quantity')
+                        discount = self._get_discount_by_article(article.get('id'))
+                        new_cart.add_article(__class__.Article(article, quantity, discount))
 
-                        if article:
-                            quantity = item.get('quantity')
-                            new_cart.add_article(__class__.Article(article, quantity))
+                self._carts.append(new_cart)
 
-                    self._carts.append(new_cart)
-
-    def get_file_from_level(self, level):
+    @staticmethod
+    def _get_path_file_by_level(level):
         import os.path
-        file = "{0}{1}level{2}/data.json".format(os.path.dirname(__file__), os.path.sep, level)
+        file = "{}/data.json".format(level)
         if not os.path.isfile(file):
             raise Exception("Level ({}) Don't exists".format(level))
 
         return file
 
-    def get_carts(self):
-        return self._carts
+    def _get_article_by_id(self, id):
+        for art in self._loaded_json.get('articles', []):
+            if art.get('id') == id:
+                return art
+        return None
 
-    def calculate_output(self):
+    def _get_discount_by_article(self, article_id):
+        for discount in self._discounts:
+            if discount.get('article_id') == article_id:
+                return discount
+        return None
+
+    def calculate_delivery_fees(self, total):
+        for delivery_fee in self._delivery_fees:
+            eligible_transaction_volume = delivery_fee.get('eligible_transaction_volume')
+
+            min_price = eligible_transaction_volume.get('min_price')
+            max_price = eligible_transaction_volume.get('max_price')
+
+            has_min_price = min_price is not None
+            has_max_price = max_price is not None
+
+            with_both_prices = has_min_price and has_max_price and min_price <= total < max_price
+            only_max_price = not has_max_price and min_price <= total
+
+            if with_both_prices or only_max_price:
+                return delivery_fee.get('price')
+
+        return 0
+
+    def create_output(self):
         output = {'carts': []}
-        for cart in self.get_carts():
-            output.get('carts').append({'id': cart.get_id(), 'total': cart.total() + cart.delivery_fees_calculated(self._delivery_fees)})
+
+        for cart in self._carts:
+            cart_total = cart.total() - cart.calculate_discounts()
+            delivery_fees_calculated = self.calculate_delivery_fees(total=cart_total)
+
+            output.get('carts').append({'id': cart.get_id(), 'total': cart_total + delivery_fees_calculated})
+
         return output
 
     def to_json(self):
-        return json.dumps(self.calculate_output())
+        return json.dumps(self.create_output(), indent=2)
 
 
-ecommerce = Ecommerce(level=2)
-ecommerce.to_json()
+if __name__ == '__main__':
+    import sys
+    if not sys.argv[1]:
+        raise Exception('Enter with a the desired level')
+
+    print(Ecommerce(level=sys.argv[1]).to_json())
